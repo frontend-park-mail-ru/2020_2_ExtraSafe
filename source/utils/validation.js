e38c7bc978d7ed0a32a2b30a50ff3f9a0a6e0c16
Egor Aistov <egoraistov@gmail.com>
2020-09-27 14:14:12 +0300

    исправлена валидация

--- source/utils/validation.js
+++ source/utils/validation.js
@@ -1,116 +1,71 @@
 "use strict"
 
-/* проверка валидности email:
-    длина не равна 0;
-    не содержит больше или меньше @ и ., чем нужно
-*/
+const emailRegExp = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$.';
+const passwordRegExp = '^[a-zA-Z0-9_+-!]$';
+const usernameRegExp = '^[a-zA-Z0-9_]$';
+const lowerCaseRegExp = '[a-z]+';
+const upperCaseRegExp = '[A-Z]+';
+const numbersRegExp = '[0-9]+';
+
+
 function validateEmail() {
-    let email = document.getElementById("email").value
-    let emailError = document.getElementById('emailError')
-
-    let at = email.split("@").length - 1;
-    let dot = email.split(".").length - 1;
-
-    if (email.length === 0) {
-        emailError.innerHTML="данное поле необходимо для заполнения";
-        emailError.hidden=false
-        return false;
-    } else if (at > 1 || dot > 1) {
-        emailError.innerHTML="не верный email";
-        emailError.hidden=false
-        return false;
-    }
-
-    emailError.innerHTML="";
-    emailError.hidden=true
-
-    return true
+    let email = document.getElementById("email").value;
+    return (new RegExp(emailRegExp)).test(email);
 }
 
-/* проверка валидности username:
-    длина не равна 0;
-    длина меньше 64 (пусть пока так будет)
-*/
-// данная функция пока (или вообще?) не используется
+
 function validateUsername() {
-    let username = document.forms[FORM_NAME]["username"].value;
+    let username = document.getElementById("username").value;
 
     if (username.length === 0) {
-        document.getElementById("usernameForm").innerHTML="данное поле необходимо для заполнения";
-        return false;
+        return {result: false, message: "обязательное поле"};
     }
 
-    if (username.length > 64) {
-        document.getElementById("usernameForm").innerHTML="максимальное количество символов - 64";
-        return false;
+    if (username.length < 2 || username.length > 40) {
+        return {result: false, message: "минимальная длина имени - 2, максимальная - 40"};
     }
 
-    document.getElementById("usernameForm").innerHTML="";
+    if (!(new RegExp(usernameRegExp)).test(username)) {
+        return {result: false, message: "содержит некорректные символы"};
+    }
+
+    return {result: true};
 }
 
-/* проверка валидности password:
-    длина не равна 0;
-    длина не меньше 8 символов;
-    пароль содержит заглавные и строчные буквы, а также цифры
-*/
+
 function validatePassword() {
-    let password = document.getElementById("password").value
-    let passwordError = document.getElementById("passwordError")
+    let password = document.getElementById("password").value;
 
     if (password.length === 0) {
-        passwordError.innerHTML="данное поле необходимо для заполнения";
-        passwordError.hidden=false
-        return false;
+        return {result: false, message: "обязательное поле"};
     }
 
-    if (password.length < 8) {
-        passwordError.innerHTML="минимальная длина пароля - 8 символов";
-        passwordError.hidden=false
-        return false;
+    if (!new RegExp(passwordRegExp).test(password)) {
+        return {result: false, message: "пароль содержит неразрешенные символы"};
     }
 
-    let upperCaseLetters = password.match(/[A-Z]/)
-    let lowerCaseLetters = password.match(/[a-z]/)
-    let numbers = password.match(/[0-9]/g)
-
-    if ((upperCaseLetters === null) || (lowerCaseLetters === null) || (numbers === null)) {
-        passwordError.innerHTML="пароль должен содержать цифры, строчные " +
-                                                                    "и заглавные буквы";
-        passwordError.hidden=false
-        return false;
+    if (!(new RegExp(lowerCaseRegExp)).test(password)) {
+        return {result: false, message: "пароль должен содержать хотя бы одну строчную букву"};
     }
 
-    passwordError.innerHTML="";
-    passwordError.hidden=true
+    if (!(new RegExp(upperCaseRegExp)).test(password)) {
+        return {result: false, message: "пароль должен содержать хотя бы одну заглавную букву"};
+    }
 
-    return true
+    if (!(new RegExp(numbersRegExp)).test(password)) {
+        return {result: false, message: "пароль должен содержать хотя бы одну цифру"};
+    }
+
+    return {result: true}
 }
 
-// проверка идентичности паролей
 function validateComparePasswords() {
     let password = document.getElementById("password").value;
     let checkPassword = document.getElementById("checkPassword").value;
-    let compareError = document.getElementById("compareError")
 
     if (password !== checkPassword) {
-        compareError.innerHTML="пароли не совпадают";
-        compareError.hidden=false
-        return false;
+        return {result: false, message: "пароли не совпадают"};
     }
 
-    if (validatePassword() !== false) {
-        compareError.innerHTML="";
-        compareError.hidden=true
-    }
-
-    return true
-}
-
-function validateSignUp() {
-    return validateEmail() && validatePassword() && validateComparePasswords()
-}
-
-
-function validateSignIn() {
-    return validateEmail() && validatePassword()
+    return {result: true}
 }
