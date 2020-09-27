6476dc7619f42c70afdf18eed34dbdcd2c37050f
Aleksandr Dergachev <smaild-m@yandex.ru>
2020-09-27 15:12:13 +0300

    Дорабатываю верстку валидации

--- source/views/RegView/RegView.js
+++ source/views/RegView/RegView.js
@@ -1,3 +1,5 @@
+"use strict"
+
 import BaseView from '../BaseView/BaseView.js';
 
 /**
@@ -23,11 +25,12 @@ export default class RegView extends BaseView {
     render() {
         this.el.innerHTML =
             `<div class="reg-container">
-                <form class="reg-form" onsubmit="alert('submit!');return false">
+                <form class="reg-form" onsubmit="updateEmailError()">
                     <div>Регистрация аккаунта</div>
                     <div class="reg-input">
                         Электронная почта:
-                        <input type="text" id="email" placeholder="mymailbox@mail.ru">
+                        <input type="text" id="email" onfocusout="updateEmailError()" placeholder="mymailbox@mail.ru" autofocus>
+                        <div id="emailError" class="reg-error" hidden="true"></div>
                     </div>
                     <div class="reg-input">
                         Полное имя:
@@ -36,7 +39,9 @@ export default class RegView extends BaseView {
                     <div class="reg-input">
                         Пароль:
                         <input id="password" placeholder="Придумайте пароль">
-                        <input id="checkPassword" placeholder="Придумайте пароль">
+                        <div id="passwordError" class="reg-error" hidden="true"></div>
+                        <input id="checkPassword" onfocusout="updatePasswordError()" placeholder="Придумайте пароль">
+                        <div id="compareError" class="reg-error" hidden="true"></div>
                     </div>
                     <input class="reg-button" type="submit" value="Зарегистрироваться">
                     <a class="reg-a" href="/login">Уже есть аккаунт? Войти!</a>
