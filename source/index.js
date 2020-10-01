c321b8a407a85b255d2c2f412641acac5990958b
Egor Aistov <egoraistov@gmail.com>
2020-10-01 22:08:36 +0300

    Добавил верстку профиля

--- source/index.js
+++ source/index.js
@@ -1,14 +1,17 @@
 import Router from './utils/router.js';
 import LoginView from './views/LoginView/LoginView.js';
 import RegView from './views/RegView/RegView.js';
+import ProfileView from "./views/ProfileView/ProfileView.js";
 
 const app = document.getElementById('application');
 const router = new Router(app);
 
 const loginView = new LoginView(app, router, {});
 const regView = new RegView(app, router, {});
+const profileView = new ProfileView(app, router, {});
 
 router.addRoute('/login', loginView);
 router.addRoute('/reg', regView);
+router.addRoute('/profile', profileView);
 
 router.open(window.location.pathname);
