67b6433f9fd47ac4df01c2718f878e0142533417
Aleksandr Dergachev <smaild-m@yandex.ru>
2020-10-02 22:29:08 +0300

    Добавил fest шаблон кнопки

--- source/views/LoginView/LoginView.js
+++ source/views/LoginView/LoginView.js
@@ -21,6 +21,19 @@ export default class LoginView extends BaseView {
      * Render Login view.
      */
     render() {
+        // const container = document.createElement('div');
+        // container.className = 'default-container';
+        // this.el.appendChild(container);
+        //
+        // const form = document.createElement('form');
+        // form.id = 'form';
+        // form.className = 'login-form';
+        // form.onsubmit = function() {
+        //     return false;
+        // };
+        // form.innerHTML += window.fest['components/SubmitButton/SubmitButton.tmpl']({buttonText: 'Войти'});
+        // container.appendChild(form);
+
         this.el.innerHTML = `<div class="default-container">
                 <form id="form" class="login-form" onsubmit="return false">
                     <div>Авторизация</div>
@@ -34,7 +47,7 @@ export default class LoginView extends BaseView {
                         <input type="password" id="password" placeholder="Введите пароль">
                         <div id="passwordError" class="login-reg-error" hidden="true"></div>
                     </div>
-                    <button type="submit" class="login-reg-button">Войти</button>
+                    ${window.fest['components/SubmitButton/SubmitButton.tmpl']({buttonText: 'Войти'})}
                     <a class="login-reg-a" href="/reg">Ещё нет аккаунта? Зарегистрироваться!</a>
                 </form>
             </div>`;
