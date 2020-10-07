423b6fe6a69dd212d5fcd09aea39042de2540a1e
Aleksandr Dergachev <smaild-m@yandex.ru>
2020-10-07 14:57:16 +0300

    Исправил ip

--- source/utils/network.js
+++ source/utils/network.js
@@ -6,7 +6,7 @@ export default class Network {
      * Constructor
      */
     constructor() {
-        this.serverAddr = 'http://127.0.0.1:8080/';
+        this.serverAddr = 'http://95.163.213.142:8080/';
     }
 
     /**
