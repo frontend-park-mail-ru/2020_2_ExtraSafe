883651e1574a837ef6dd645a0ddb707dee054335
Aleksandr Dergachev <smaild-m@yandex.ru>
2020-11-05 15:32:39 +0300

    Исправления после слияния

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static(`${__dirname}/source`));
 
-const port = 3033;
+const port = 80;
 
 app.get('*', (req, res) => {
     res.sendFile(`${__dirname}/source/index.html`);
