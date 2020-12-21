34eeabdc824d909959b60e5ed6e356ea1d2e04e3
Aleksandr Dergachev <smaild-m@yandex.ru>
2020-12-21 18:21:34 +0300

    Исправляю ошибки cicd

--- src/utils/network.js
+++ src/utils/network.js
@@ -10,7 +10,7 @@ class Network {
     constructor() {
         this.frontAddr = 'http://tabutask.ru';
         this.serverAddr = 'http://tabutask.ru';
-        // this.serverAddrWS = 'ws://127.0.0.1:8080';
+        this.serverAddrWS = 'ws://tabutask.ru';
         // this.frontAddr = 'http://127.0.0.1';
         // this.serverAddr = 'http://127.0.0.1:8080';
         // this.serverAddrWS = 'ws://127.0.0.1:8080';
