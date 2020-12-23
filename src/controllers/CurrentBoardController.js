255af89a105d6701b125573bf1f0c6b725741245
Aleksandr Dergachev <smaild-m@yandex.ru>
2020-12-23 20:20:56 +0300

    Исправил ошибку с порядком карточек

--- src/controllers/CurrentBoardController.js
+++ src/controllers/CurrentBoardController.js
@@ -99,7 +99,7 @@ export default class CurrentBoardController extends BaseController {
             card.model.card.order = cardIndex;
             cards.push({
                 cardID: card.model.card.cardID,
-                order: card.model.card.order,
+                order: card.model.card.cardOrder,
             });
 
             this.view.updateCardOrder(card.model.card.cardHtmlID, cardIndex);
